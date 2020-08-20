4d9f853faa11c321d5abca7eb8b22e25c7fee65d
Mari <ak_mariya@tut.by>
2020-08-20 19:42:58 +0300

    Soap

--- fixture/soap.py
+++ fixture/soap.py
@@ -6,11 +6,12 @@ from model.project import Project
 
 class SoaHelper:
 
-    def __init__(self,app):
+    def __init__(self, app):
         self.app = app
+        self.base_url = app.base_url
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-2.24.1/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -18,7 +19,7 @@ class SoaHelper:
             return False
 
     def get_project_list(self, username, password):
-        client = Client("http://localhost/mantisbt-2.24.1/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.base_url + "/api/soap/mantisconnect.php?wsdl")
         list_projects = client.service.mc_projects_get_user_accessible(username, password)
         projects = []
         for project in list_projects:
